61b64a008e0508ef639fac4a237a779d3ac3c416
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-05-21 22:47:15 +0100

    add error messages while creating new task

--- views.py
+++ views.py
@@ -33,6 +33,14 @@ def flash_errors(form):
         for error in errors:
             flash("Error in the {} field - {}".format(getattr(form, field).label.text, error), 'error')
 
+
+def open_tasks():
+    return db.session.query(Task).filter_by(status='1').order_by(Task.due_date.asc())
+
+
+def closed_tasks():
+    return db.session.query(Task).filter_by(status='0').order_by(Task.due_date.asc())
+
 ########################
 #### route handlers ####
 ########################
@@ -85,20 +93,18 @@ def login():
 @app.route('/tasks/')
 @login_required
 def tasks():
-    open_tasks = db.session.query(Task)\
-            .filter_by(status='1').order_by(Task.due_date.asc())
-    closed_tasks = db.session.query(Task)\
-            .filter_by(status='0').order_by(Task.due_date.asc())
     return render_template(
         'tasks.html',
         form=AddTaskForm(request.form),
-        open_tasks=open_tasks,
-        closed_tasks=closed_tasks
+        open_tasks=open_tasks(),
+        closed_tasks=closed_tasks()
     )
+
 
 @app.route('/add', methods=['GET','POST'])
 @login_required
 def new_task():
+    error = None
     form = AddTaskForm(request.form)
     if request.method == 'POST':
         if form.validate_on_submit():
@@ -113,9 +119,14 @@ def new_task():
             db.session.add(new_task)
             db.session.commit()
             flash('New entry was successfully posted. Thanks.')
-        else:
-            flash('Please provide complete data')
-    return redirect(url_for('tasks'))
+            return redirect(url_for('tasks'))
+    return render_template(
+        'tasks.html',
+        form=form,
+        error=error,
+        open_tasks=open_tasks(),
+        closed_tasks=closed_tasks()
+    )
 
 
 @app.route('/complete/<int:task_id>')
